04d9ffe2feac0fd489513201d0999fd52c93b1ef
vincentcarbonaro <nd4spd356@gmail.com>
2015-05-13 18:02:15 -0400

    Retrieved part 2 data

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -12,6 +12,14 @@ class StaticPagesController < ApplicationController
       @results << "#{listing.at_xpath('StreetAddress').text} #{listing.at_xpath('City').text} #{listing.at_xpath('State').text} #{listing.at_xpath('Zip').text}"
     end
 
+    doc = Nokogiri::HTML(open("http://www.corcoran.com/nyc/Search/Listings?SaleType=Rent"))
+
+    @results2 = []
+
+    doc.css('.info').each do |anchor|
+      @results2 << anchor.css('.address').text.strip + anchor.css('.hood').text.strip
+    end
+
   end
 
 end
