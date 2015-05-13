f1dc31f2b25c5401c58ccf37e24b1660f9e2a7d2
vincentcarbonaro <nd4spd356@gmail.com>
2015-05-13 00:52:32 -0400

    Part 1 XML retrieved

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,17 @@
 class StaticPagesController < ApplicationController
 
   def root
+    require 'nokogiri'
+    require 'open-uri'
+
+    doc = Nokogiri::XML(open("http://www.related.com/feeds/ZillowAvailabilities.xml"))
+
+    @results = []
+
+    doc.xpath('//Listing/Location').each do |listing|
+      @results << "#{listing.at_xpath('StreetAddress').text} #{listing.at_xpath('City').text} #{listing.at_xpath('State').text} #{listing.at_xpath('Zip').text}"
+    end
+
   end
 
 end
