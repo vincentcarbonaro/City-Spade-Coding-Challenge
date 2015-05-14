7f5ccf162e56e49e6ee246b59d95efc0ced41c01
vincentcarbonaro <nd4spd356@gmail.com>
2015-05-14 00:37:31 -0400

    Fixing Bugs

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -51,4 +51,51 @@ class StaticPagesController < ApplicationController
 
   end
 
+  def FULLUPDATE
+    require 'nokogiri'
+    require 'open-uri'
+
+    ################################################################################
+
+    doc = Nokogiri::XML(open("http://www.related.com/feeds/ZillowAvailabilities.xml"))
+
+    @results = []
+
+    doc.xpath('//Listing/Location').each do |listing|
+
+      if location = Location.find_by_pid(listing.at_xpath('ZPID').text)
+      else
+        location = Location.new(pid: listing.at_xpath('ZPID').text, address: "#{listing.at_xpath('StreetAddress').text} #{listing.at_xpath('City').text} #{listing.at_xpath('State').text} #{listing.at_xpath('Zip').text}")
+        location.save!
+      end
+
+      @results << location
+
+    end
+
+    ################################################################################
+
+    url = "http://www.corcoran.com/nyc/Search/Listings?SaleType=Rent&&Count=36&Page="
+    i = 24
+    @results2 = []
+
+    while Nokogiri::HTML(open("#{url}#{i}")).css('.info').length > 0
+
+      doc = Nokogiri::HTML(open("#{url}#{i}"))
+
+      doc.css('.listing').each do |listing|
+
+        if location = Location.find_by_pid(listing.attributes['data-listingid'].text)
+        else
+          location = Location.new(pid: listing.attributes['data-listingid'].text, address: listing.css('.address').text.strip + " " + listing.css('.hood').text.strip)
+          location.save!
+        end
+
+        @results2 << location
+      end
+    #
+      i+=1
+    end
+  end
+
 end
