648909974b7bf76ccd5737a68c4da79608486d3e
vincentcarbonaro <nd4spd356@gmail.com>
2015-05-14 16:50:45 -0400

    debug

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,6 +1,5 @@
 Geocoder.configure(
+  use_https: true,
   http_proxy: "http://quotaguard2720:700399cf9303@proxy.quotaguard.com:9292",
-  # api_key: "AIzaSyDzJuQL5GR4pzHRpsXlZZ9Z4_soBHGlXys",
   timeout:  5,
-  # ip_lookup: :telize
 )
