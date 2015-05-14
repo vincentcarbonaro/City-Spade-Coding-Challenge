becd551849cfeefa356b53c646036033981c15d8
vincentcarbonaro <nd4spd356@gmail.com>
2015-05-14 16:28:16 -0400

    Debugging

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,4 +1,5 @@
 Geocoder.configure(
-  :http_proxy => ENV['QUOTAGUARD_URL'],
-  :timeout => 5
+  http_proxy: "http://quotaguard2720:700399cf9303@proxy.quotaguard.com:9292",
+  timeout:  5,
+  ip_lookup: telize
 )
