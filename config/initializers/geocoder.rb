a02c0d937e089864f34ac916a0a59ee2c1ff9a2a
vincentcarbonaro <nd4spd356@gmail.com>
2015-05-14 17:00:18 -0400

    debug

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,4 +1,5 @@
 Geocoder.configure(
+  :api_key => "AIzaSyC9j3kO_j_nC345l4XGQwow3Q8OvtlDZAA",
   use_https: true,
   http_proxy: "http://quotaguard2720:700399cf9303@proxy.quotaguard.com:9292",
   timeout:  5,
