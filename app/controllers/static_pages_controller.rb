99042c8885356be93bfea23308db38e0f9d13b7d
vincentcarbonaro <nd4spd356@gmail.com>
2015-05-14 18:10:47 -0400

    debug

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -33,7 +33,7 @@ class StaticPagesController < ApplicationController
     ###############################################################################
 
     url = "http://www.corcoran.com/nyc/Search/Listings?SaleType=Rent&&Count=36&Page="
-    i = 26
+    i = 0
     @results2 = []
 
     while Nokogiri::HTML(open("#{url}#{i}")).css('.info').length > 0
@@ -57,56 +57,4 @@ class StaticPagesController < ApplicationController
       i+=1
     end
   end
-
-  def FULLUPDATE
-    require 'nokogiri'
-    require 'open-uri'
-
-    doc = Nokogiri::XML(open("http://www.related.com/feeds/ZillowAvailabilities.xml"))
-
-    @results = []
-
-    doc.xpath('//Listing/Location').each do |listing|
-
-      if location = Location.find_by_pid(listing.at_xpath('ZPID').text)
-        if location.latitude == nil
-          location.address = location.address + " "
-          location.save!
-          sleep(0.2)
-        end
-      else
-        pid = listing.at_xpath('ZPID').text
-        address = "#{listing.at_xpath('StreetAddress').text} #{listing.at_xpath('City').text} #{listing.at_xpath('State').text} #{listing.at_xpath('Zip').text}"
-        location = Location.new(pid: pid, address: address)
-        location.save!
-        sleep(0.2)
-      end
-      @results << location
-    end
-
-    ###############################################################################
-
-    url = "http://www.corcoran.com/nyc/Search/Listings?SaleType=Rent&&Count=36&Page="
-    @results2 = []
-
-    while Nokogiri::HTML(open("#{url}#{i}")).css('.info').length > 0
-      doc = Nokogiri::HTML(open("#{url}#{i}"))
-
-      doc.css('.listing').each do |listing|
-        if location = Location.find_by_pid(listing.attributes['data-listingid'].text)
-          if location.latitude == nil
-            location.address = location.address + " "
-            location.save!
-            sleep(0.2)
-          end
-        else
-          location = Location.new(pid: listing.attributes['data-listingid'].text, address: listing.css('.address').text.strip + " " + listing.css('.hood').text.strip)
-          sleep(0.2)
-          location.save!
-        end
-        @results2 << location
-      end
-    end
-  end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,5 @@ Rails.application.routes.draw do
 
   get '/update' => 'static_pages#update'
   get '/index' => "static_pages#index"
-  get '/FULL_UPDATE' => "static_pages#FULLUPDATE"
 
 end
