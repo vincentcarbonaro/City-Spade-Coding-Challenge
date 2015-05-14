026e71071b694118197bc6e854a1afad23e9009d
vincentcarbonaro <nd4spd356@gmail.com>
2015-05-14 17:15:58 -0400

    debug

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -11,29 +11,22 @@ class StaticPagesController < ApplicationController
     doc = Nokogiri::XML(open("http://www.related.com/feeds/ZillowAvailabilities.xml"))
 
     @results = []
-    z = 0;
 
     doc.xpath('//Listing/Location').each do |listing|
-
 
       if location = Location.find_by_pid(listing.at_xpath('ZPID').text)
         if location.latitude == nil
           location.address = location.address + " "
           location.save!
         end
-        @results << location
       else
-
         pid = listing.at_xpath('ZPID').text
         address = "#{listing.at_xpath('StreetAddress').text} #{listing.at_xpath('City').text} #{listing.at_xpath('State').text} #{listing.at_xpath('Zip').text}"
         location = Location.new(pid: pid, address: address)
         location.save!
         sleep(0.2)
-        @results << location
-
       end
-
-      z+=1
+      @results << location
 
     end
 
@@ -45,45 +38,41 @@ class StaticPagesController < ApplicationController
     @results2 = []
 
     while Nokogiri::HTML(open("#{url}#{i}")).css('.info').length > 0
-
       doc = Nokogiri::HTML(open("#{url}#{i}"))
 
       doc.css('.listing').each do |listing|
-        location = Location.new(pid: listing.attributes['data-listingid'].text, address: listing.css('.address').text.strip + " " + listing.css('.hood').text.strip)
-        sleep(0.2)
-        location.save!
-        @results2 << location
+        if location = Location.find_by_pid(listing.attributes['data-listingid'].text)
+          location = Location.new(pid: listing.attributes['data-listingid'].text, address: listing.css('.address').text.strip + " " + listing.css('.hood').text.strip)
+          sleep(0.2)
+          location.save!
+          @results2 << location
+        end
       end
-
       i+=1
     end
-
-    # Users of the free API:
-    # 2,500 requests per 24 hour period.
-    # 5 requests per second.
-
   end
-
-  ############################################################################################################
 
   def FULLUPDATE
     require 'nokogiri'
     require 'open-uri'
-    require 'net/http' #needed for making requests (to google api)
-    require 'json' #needed for parsing JSON responses
 
     doc = Nokogiri::XML(open("http://www.related.com/feeds/ZillowAvailabilities.xml"))
-
     @results = []
 
     doc.xpath('//Listing/Location').each do |listing|
-
       if location = Location.find_by_pid(listing.at_xpath('ZPID').text)
+        if location.latitude == nil
+          location.address = location.address + " "
+          location.save!
+          sleep(0.2)
+        end
       else
-        location = Location.new(pid: listing.at_xpath('ZPID').text, address: "#{listing.at_xpath('StreetAddress').text} #{listing.at_xpath('City').text} #{listing.at_xpath('State').text} #{listing.at_xpath('Zip').text}")
+        pid = listing.at_xpath('ZPID').text
+        address = "#{listing.at_xpath('StreetAddress').text} #{listing.at_xpath('City').text} #{listing.at_xpath('State').text} #{listing.at_xpath('Zip').text}"
+        location = Location.new(pid: pid, address: address)
         location.save!
+        sleep(0.2)
       end
-
       @results << location
 
     end
@@ -91,7 +80,6 @@ class StaticPagesController < ApplicationController
     ################################################################################
 
     url = "http://www.corcoran.com/nyc/Search/Listings?SaleType=Rent&&Count=36&Page="
-    i = 22
     @results2 = []
 
     while Nokogiri::HTML(open("#{url}#{i}")).css('.info').length > 0
@@ -99,19 +87,20 @@ class StaticPagesController < ApplicationController
       doc = Nokogiri::HTML(open("#{url}#{i}"))
 
       doc.css('.listing').each do |listing|
-
         if location = Location.find_by_pid(listing.attributes['data-listingid'].text)
+          location.address = location.address + " "
+          location.save!
+          sleep(0.2)
         else
           location = Location.new(pid: listing.attributes['data-listingid'].text, address: listing.css('.address').text.strip + " " + listing.css('.hood').text.strip)
           location.save!
-          fail
+          sleep(0.2)
         end
-
         @results2 << location
       end
-
       i+=1
     end
+
   end
 
 end
